2c4807618301077147d203cc8ac719b1a661f8ef
Jonathan Mohon <jonathan@webjoint.com>
2021-07-06 12:57:14 -0700

    Navigation Update
    Navigation Update

--- App.js
+++ App.js
@@ -1,28 +1,14 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { View, Text } from 'react-native';
-// import { HomeScreen } from './Components/Home';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { AppNavigator } from './Components/Navigation';
 
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default App;
+export default () => (
+  <>
+    <IconRegistry icons={EvaIconsPack}/>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <AppNavigator/>
+    </ApplicationProvider>
+  </>
+);
